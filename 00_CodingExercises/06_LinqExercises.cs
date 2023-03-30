55b7266058b370d105c7d6f71eb0083674c9f35b
krystyna.slusarczyk.learning@gmail.com <krystyna.slusarczyk.learning@gmail.com>
2023-03-30 14:56:53 +0200

    update LINQ Any and All exercise

--- 00_CodingExercises/06_LinqExercises.cs
+++ 00_CodingExercises/06_LinqExercises.cs
@@ -1,9 +1,9 @@
 ﻿public class LinqExercises
 {
     //Any & All
-   public static bool IsAnyWordUppercase(List<string> words) =>
-        words.Any(word => word.All(
-            character => char.IsUpper(character)));
+    public static bool IsAnyWordWhitespace(List<string> words) =>
+         words.Any(word => word.All(
+             character => char.IsWhiteSpace(character)));
 
     //Count & Contains
     public static int CountListsContainingZeroLongerThan(
